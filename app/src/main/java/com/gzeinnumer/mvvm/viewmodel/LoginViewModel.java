a4ec77d0b37bd4dde90811d27fe8498dd7fce423
M. Fadli Zein <m.fadli.zein.17@gmail.com>
2019-02-28 10:43:17 +0700

    Initial commit

---
+++ app/src/main/java/com/gzeinnumer/mvvm/viewmodel/LoginViewModel.java
@@ -0,0 +1,50 @@
+package com.gzeinnumer.mvvm.viewmodel;
+
+import android.databinding.BaseObservable;
+import android.databinding.Bindable;
+
+import com.gzeinnumer.mvvm.BR;
+import com.gzeinnumer.mvvm.Model.User;
+
+public class LoginViewModel extends BaseObservable {
+
+    private User user;
+
+
+    private String successMessage = "Login was successful";
+    private String errorMessage = "Email or Password not valid";
+
+    @Bindable
+    public String toastMessage = null;
+
+
+    public String getToastMessage() {
+        return toastMessage;
+    }
+
+
+    private void setToastMessage(String toastMessage) {
+
+        this.toastMessage = toastMessage;
+        notifyPropertyChanged(BR.toastMessage);
+    }
+
+    public LoginViewModel() {
+        user = new User("", "");
+    }
+
+    public void afterEmailTextChanged(CharSequence s) {
+        user.setEmail(s.toString());
+    }
+
+    public void afterPasswordTextChanged(CharSequence s) {
+        user.setPassword(s.toString());
+    }
+
+    public void onLoginClicked() {
+        if (user.isInputDataValid())
+            setToastMessage(successMessage);
+        else
+            setToastMessage(errorMessage);
+    }
+}
